76f9396824593419125e8a85e9ce0e3616a6ceea
Denali101 <80551417+Denali101@users.noreply.github.com>
2021-03-28 16:42:36 -0400

    Update J3_Secret_Instructions.py

--- J3_Secret_Instructions.py
+++ J3_Secret_Instructions.py
@@ -1,16 +1,18 @@
-previous = ""
+previous = "" # For when the first two nums = 0
 
 while True:
-    nums = input()
+    nums = input() # Has to be string because there can be 0 in the beginning
     if nums == "99999":
         break
-    choice = int(nums[0]) + int(nums[1])
+    choice = int(nums[0]) + int(nums[1]) # Adds the first two nums of the input
     direction = ""
     if choice == 0:
-        direction = previous
-    elif choice % 2 != 0:
+        direction = previous # Because the 00___ only occurs after another number that is not 00___, the direction variable would have already been updated, meaning we can just use that.
+    elif choice % 2 != 0: # Odd nums
         direction = "left"
-    elif choice % 2 == 0:
+    elif choice % 2 == 0: # Even nums
         direction = "right"
-    previous = direction
+    previous = direction # The previous variable needs to be updated when we loop again
     print(direction + " " + nums[2:])
+    
+  
